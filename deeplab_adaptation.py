b7cc7c941671c6250a6537c304e3f9dc41f0c7a7
the-cyclops <enrico.loda01@gmail.com>
2025-08-11 00:29:26 +0200

    setup to train with lower mmd

--- deeplab_adaptation.py
+++ deeplab_adaptation.py
@@ -717,7 +717,7 @@ def main():
     BATCH_SIZE = 4
     start_epoch = 0
     end_epochs = 5  # Epochs to end train
-    MMD_WEIGHT = 0.1
+    MMD_WEIGHT = 0.05
     CE_IMPORTANCE = 0.7
     
     # Set device
@@ -744,7 +744,7 @@ def main():
     best_model_path = "models/deeplabv3_imagenet1k_best_model.pth"
 
     # Model name for saving
-    name = "DA_deeplabv3_imagenet1k"
+    name = "DA_lowerMMD_deeplabv3_imagenet1k"
 
     model = create_model(
         num_classes=NUM_CLASSES, 
